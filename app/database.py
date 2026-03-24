8a1d4a5ea3df49c0ffa8b164dd71b400e91ceaf6
Sairam-M <sairamm3110@gmail.com>
2026-03-24 15:44:24 +0530

    Add CSV upload functionality and demographics retrieval API

--- app/database.py
+++ app/database.py
@@ -1,4 +1,7 @@
 # Connect with Postgres SQL Database using SQLAlchemy
+from http.client import HTTPException
+
+from fastapi import logger
 from sqlalchemy import create_engine, text
 
 DATABASE_URL = "postgresql://postgres:Sql123@localhost/nfhs_db"
@@ -9,8 +12,10 @@ def get_demographics_data():
         result = connection.execute(text("SELECT * FROM demographics"))
         return result.fetchall()
 
-# Create __main__ block to test the connection
-if __name__ == "__main__":
-    data = get_demographics_data()
-    print(type(data))
-    print(data)
+def upload_demographics_data(df):
+    with engine.connect() as connection:
+        try:
+            df.to_sql("demographics", connection, if_exists="append", index=False)
+        except Exception as e:
+            logger.error(f"Failed to upload demographics data: {e}")
+            raise HTTPException(status_code=500, detail="Failed to upload demographics data")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, UploadFile
+from csv import DictReader
+from .database import get_demographics_data, upload_demographics_data
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "NFHS Analytics API is running!"}
+
+
+# API to upload csv file
+@app.post("/upload")
+async def upload_csv(file: UploadFile):
+    if not file.filename.endswith('.csv'):
+        # raise HTTPException with appropriate status code
+        raise HTTPException(status_code=400, 
+                            detail="Invalid file type. Please upload a CSV file.")
+    # Handle empty File
+    try:
+        df = pd.read_csv(file.file)
+    except Exception as e:
+        # Raise HTTPException with appropriate status code and error message
+        raise HTTPException(status_code=400, 
+                            detail="Failed to process CSV file")
+
+    row_count = len(df)
+    upload_demographics_data(df)
+    return {"message": "CSV file uploaded successfully!"}
+
+@app.get("/demographics")
+def get_demographics():
+    data = get_demographics_data()
+    result = []
+    for row in data:
+        result.append({
+            "id": row[0],
+            "state": row[1],
+            "anemia_women" : row[2],
+            "female_education_years": row[3],
+            "bmi_low": row[4],
+            "rural_population": row[5],
+            "child_morality_rate": row[6]
+        })
+    return {"data": result}
